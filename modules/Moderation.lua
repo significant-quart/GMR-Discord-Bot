69a350aa3c5a51f4f2479de9a56b6603d4cda7b6
AntwanR942 <antwang942@gmail.com>
2021-12-07 16:29:07 +0000

    readd debug

--- modules/Moderation.lua
+++ modules/Moderation.lua
@@ -270,6 +270,7 @@ BOT:on("memberJoin", HandleMemberName)
 
 --[[ Interval ]]
 Interval((Config["DefaultInterval"] * 4) * 1000, function()
+    print("...", GMRG, GMRG.members)
     if not GMRG or not GMRG.members then return end
 
     for _, Member in pairs(GMRG.members) do
